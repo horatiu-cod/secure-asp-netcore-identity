55a36f7c650147dec906ee06c4a78a189412892f
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-03 11:19:30 +0000

    Run codegenerator tool to scafold identity
    Support for ASP.NET Core Identity was added to your project.

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPizza.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection") ?? throw new InvalidOperationException("Connection string 'RazorPagesPizzaAuthConnection' not found.");
+
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
